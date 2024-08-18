6337a255b581e2bb612eaf6c3a89532dec205b0b
Uzair Naeem <uzairnaeem15@gmail.com>
2024-08-18 00:25:56 -0500

    added base url in vite config

--- frontend/jobsearchfe/vite.config.js
+++ frontend/jobsearchfe/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/JobSearchAI/', // Update this to your GitHub repository name
 })
