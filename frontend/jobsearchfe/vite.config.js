cf20a3a5c430dc13e07ce631e0580a2f32291351
Uzair Naeem <uzairnaeem15@gmail.com>
2024-08-17 22:55:04 -0500

    deployment files changes

--- frontend/jobsearchfe/vite.config.js
+++ frontend/jobsearchfe/vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/JobSearchAI/', // Update this to your GitHub repository name
 })
